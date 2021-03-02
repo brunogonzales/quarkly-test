504d15825e614a523430abb7e3ea01eb434a2287
Bruno Gonzales <57231813+brunogonzales@users.noreply.github.com>
2021-03-02 14:48:09 -0500

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"603e40ebc3a5f1001e7b808c",
+				"603e40ebc3a5f1001e7b808e"
+			]
+		},
+		"603e40ebc3a5f1001e7b808c": {
+			"id": "603e40ebc3a5f1001e7b808c",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"603e40ebc3a5f1001e7b808e": {
+			"id": "603e40ebc3a5f1001e7b808e",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
